8db944aa2bb8e41ab390d790e2e3f7fd0dcb8ff4
pophils <seunpopoola@outlook.com>
2014-03-26 08:42:41 +0100

    slight modification

--- src/trunk/MessageBoard/MessageBoard.UI.Web/Filters/CustomActionFilter.cs
+++ src/trunk/MessageBoard/MessageBoard.UI.Web/Filters/CustomActionFilter.cs
@@ -3,10 +3,7 @@ using System.Web.Mvc;
 
 namespace MessageBoard.UI.Web.Filters
 {
-    /// <summary>
-    /// This class will intercept requests before they are executed and log them. 
-    /// Ref: Owolabi http://www.asp.net/mvc/tutorials/hands-on-labs/aspnet-mvc-4-custom-action-filters
-    /// </summary>
+    
     public class MbCustomActionFilter : ActionFilterAttribute, IActionFilter
     {
         void IActionFilter.OnActionExecuting(ActionExecutingContext filterContext)
